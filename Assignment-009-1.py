6dbb6f24459f85ecbcf4a7470a30720bf2e0ab18
burakagcakaya <agcakayaburak@gmail.com>
2021-07-19 20:21:35 +0300

    version 1.0.1

---
+++ Assignment-009-1.py
@@ -0,0 +1,6 @@
+x = "Burak"
+y = input("Please enter your name : ").title().strip()
+if x == y:
+    print("Hello, Burak! The password is : W@12",)
+else :
+    print( "Hello, {} See you later.".format(y))
